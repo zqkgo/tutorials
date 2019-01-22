0d5cbd2e706b617b51fd02658356b73e1e17f50f
kongoole <1160104271@qq.com>
2019-01-22 10:30:46 +0800

    add dfs iteration version

--- go/algo/tree/dfs_iteration.go
+++ go/algo/tree/dfs_iteration.go
@@ -2,22 +2,15 @@ package main
 
 // 10 6 4 2 5 8 7 9 14 12 11 13 16 15 17
 func PreOrderInteration(root *Node) {
-	// var queue []*Node
-	// visit(root)
-	// queue = append(queue, root)
-	// for len(queue) != 0 {
-	// 	cur := queue[0]
-	// 	queue = queue[1:]
-	// 	visit(cur)
-	// 	if cur.Left != nil {
-	// 		queue = append(queue, cur.Left)
-	// 	}
-	// 	if cur.Right != nil {
-	// 		queue = append(queue, cur.Right)
-	// 	}
-	// }
 }
 
+//         10
+//       /     \
+//    6         14
+//   / \       /  \
+//  4   8    12    16
+// / \ / \  / \   / \
+// 2 5 7 9 11 13 15 17
 // 2 4 5 6 7 8 9 10 11 12 13 14 15 16 17
 func InOrderInteration(root *Node) {
 	var stack []*Node
@@ -37,11 +30,4 @@ func InOrderInteration(root *Node) {
 
 // 2 5 4 7 9 8 6 11 13 12 15 17 16 14 10
 func PostOrderInteration(root *Node) {
-	if root.Left != nil {
-		PostOrder(root.Left)
-	}
-	if root.Right != nil {
-		PostOrder(root.Right)
-	}
-	visit(root)
 }
